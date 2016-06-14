af9aeaabd7392ed91a2f1994c0affa7b7fd984d2
helenzhangg <h.zhang2021@gmail.com>
2016-06-14 00:02:41 -0400

    added files for connecting Firebase data to user posts

---
+++ tamidShowcase/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  tamidShowcase
+//
+//  Created by H on 5/24/16.
+//  Copyright © 2016 H. All rights reserved.
+//
+
+import Foundation
---
+++ tamidShowcase/FeedVC.swift
@@ -0,0 +1,37 @@
+//
+//  FeedVC.swift
+//  tamidShowcase
+//
+//  Created by H on 5/31/16.
+//  Copyright © 2016 H. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+
+}
